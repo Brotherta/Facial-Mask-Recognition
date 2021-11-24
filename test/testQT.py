ab4d783356b5e42542f9a494dfbcb4e7c1f7e2a0
Brotherta <antoine.vidalma@gmail.com>
2021-11-24 12:14:14 +0100

    change qt for pySide6

--- test/testQT.py
+++ test/testQT.py
@@ -1,7 +1,32 @@
-from PyQt5.QtWidgets import QApplication, QLabel
+import sys
+import random
+from PySide6 import QtCore, QtWidgets, QtGui
 
-app = QApplication([])
+class MyWidget(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
 
-label = QLabel('Hello World!')
-label.show()
-app.exec_()
+        self.hello = ["Hallo Welt", "Hei maailma", "Hola Mundo", "Привет мир"]
+
+        self.button = QtWidgets.QPushButton("Click me!")
+        self.text = QtWidgets.QLabel("Hello World",
+                                     alignment=QtCore.Qt.AlignCenter)
+
+        self.layout = QtWidgets.QVBoxLayout(self)
+        self.layout.addWidget(self.text)
+        self.layout.addWidget(self.button)
+
+        self.button.clicked.connect(self.magic)
+
+    @QtCore.Slot()
+    def magic(self):
+        self.text.setText(random.choice(self.hello))
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication([])
+
+    widget = MyWidget()
+    widget.resize(800, 600)
+    widget.show()
+
+    sys.exit(app.exec())
